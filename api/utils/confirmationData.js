47129f433dc2db907f579d2923f358488313ffb7
Solorzano <Rafael.Solorzano@gov.bc.ca>
2020-10-15 15:28:25 -0700

    added initial api
    added initial api folder - skeleton code

---
+++ api/utils/confirmationData.js
@@ -0,0 +1,20 @@
+var strings = require("./strings")
+
+module.exports = {
+    getSubmitted(values){
+        var data = [
+            `The following information was received:`,
+            `<hr />`,
+            `<hr />`,
+        ]
+        return data
+
+    },
+    getClaimSubmitted(values){
+        var data=[
+            ``,
+        ]
+        return data
+
+    }
+}
