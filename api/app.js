ab3820828baf48c4d71afa3de393af509d8ed9c4
Solorzano <Rafael.Solorzano@gov.bc.ca>
2020-10-16 16:58:33 -0700

    finished api skeleton

---
+++ api/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var helmet = require('helmet');
+var csrf = require('csurf')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+var origin = process.env.ORIGIN_URL || process.env.OPENSHIFT_NODEJS_ORIGIN_URL || "http://localhost:3000"
+
+const corsOptions = {
+    origin: origin,
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
+
+var formRouter = require('./routes/form')
+//var participantFormRouter = require('./routes/participantForm')
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser());
+app.use(helmet());
+
+
+app.use('/api/form', formRouter)
+//app.use('/api/participantForm',participantFormRouter)
+
+module.exports = app;
