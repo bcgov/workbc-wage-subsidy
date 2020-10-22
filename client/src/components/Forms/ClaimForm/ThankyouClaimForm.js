9bd60c0b493df167d4e9a54367619b124e206248
Wes R <kohopolo@gmail.com>
2020-10-22 13:36:59 -0700

    Added route for ThankyouClaimForm

---
+++ client/src/components/Forms/ClaimForm/ThankyouClaimForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ThankyouClaimForm extends Component {
+    render() {
+        return (
+            <h1>Hey there, I work!</h1>
+        );
+    }
+}
+
+export default ThankyouClaimForm
