7540b3b942a043cf4394942ec756713e9b7301d9
Wes R <kohopolo@gmail.com>
2020-10-28 14:00:24 -0700

    Add alert and component mount hook to calim form

--- client/src/constants/form.js
+++ client/src/constants/form.js
@@ -8,7 +8,9 @@ export const FORM_URL = {
 if (process.env.NODE_ENV === 'development'){
     FORM_URL.haveEmployeeForm = 'http://localhost:8000/api/haveEmployeeForm'
     FORM_URL.clientForm = 'http://localhost:8000/api/participantForm'
+    FORM_URL.claimForm = 'http://localhost:8000/api/claimForm'
 } else if (process.env.NODE_ENV === 'production') {
     FORM_URL.mainForm = '/api/form'
     FORM_URL.clientForm = '/api/participantForm'  
+    FORM_URL.claimForm = '/api/clientForm'
 }
