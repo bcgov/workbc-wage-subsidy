0b3918ddc68f97e4fbde4d48d4318f209d93cc71
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2020-11-09 15:46:15 -0800

    updated claim form url, removed unused

--- client/src/constants/form.js
+++ client/src/constants/form.js
@@ -11,9 +11,8 @@ if (process.env.NODE_ENV === 'development'){
     FORM_URL.clientForm = 'http://localhost:8000/api/participantForm'
     FORM_URL.claimForm = 'http://localhost:8000/api/claimForm'
 } else if (process.env.NODE_ENV === 'production') {
-    FORM_URL.mainForm = '/api/form'
     FORM_URL.haveEmployeeForm = '/api/haveEmployeeForm'
     FORM_URL.needEmployeeForm = '/api/needEmployeeForm'
     FORM_URL.clientForm = '/api/participantForm'  
-    FORM_URL.claimForm = '/api/clientForm'
+    FORM_URL.claimForm = '/api/claimForm'
 }
