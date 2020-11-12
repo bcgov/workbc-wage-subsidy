cd7b820990150d2876eb6cd99925d317c809ba09
Dluhosh <Whitney.Dluhosh@gov.bc.ca>
2020-11-12 14:09:49 -0800

    next button

--- client/src/components/Forms/HaveEmployeeForm/HaveEmployeeForm.js
+++ client/src/components/Forms/HaveEmployeeForm/HaveEmployeeForm.js
@@ -210,34 +210,34 @@ class HaveEmployeeForm extends Component {
                                     credentials: 'include',
                                     headers: {
                                         'Accept': 'application/json',
-                                        'Content-Type' : 'application/json',
+                                        'Content-Type':'application/json',
                                     },
                                     body: JSON.stringify(values),
                                 })
                                 
-                                .then(res => res.json())
-                                .then(
-                                    (resp) => {
-                                        console.log(resp)
+                                    .then(res => res.json())
+                                    .then(
+                                        resp => {
+                                            console.log(resp)
 
-                                        if (resp.err){
-                                            console.log("errors", resp)
-                                            setSubmitting(false)
-                                            setErrors(resp.err)
+                                            if (resp.err){
+                                                console.log("errors", resp)
+                                                setSubmitting(false)
+                                                setErrors(resp.err)
 
-                                        } else if(resp.emailErr){
-                                            console.log("emailError", resp)
-                                            setSubmitting(false)
-                                            this.setState({
-                                                hasError: true
-                                            })
-                                        } 
-                                        else if (resp.ok){
-                                            setSubmitting(false)
-                                            this.props.history.push('/thankYouHaveEmployee',values)
+                                            } else if(resp.emailErr){
+                                                console.log("emailError", resp)
+                                                setSubmitting(false)
+                                                this.setState({
+                                                    hasError: true
+                                                })
+                                            } 
+                                            else if (resp.ok){
+                                                setSubmitting(false)
+                                                this.props.history.push('/thankYouHaveEmployee',values)
+                                            }
                                         }
-                                    }
-                                )
+                                    )
                                 
                                 
                             }}
