1baeb830926edca50d91cf538d3ae49e68d01064
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2021-01-18 14:59:43 -0800

    Update FormStep1.js
    removed console log

--- client/src/components/Forms/shared/FormStep1.js
+++ client/src/components/Forms/shared/FormStep1.js
@@ -280,7 +280,6 @@ class FormStep1 extends Component {
         return (
 
             <div>
-                {console.log(this.props)}
                 <p>
                     If you are having difficulty completing the application for Wage Subsidy, please contact your local <a href="https://www.workbc.ca/Employment-Services/WorkBC-Centres/Find-Your-WorkBC-Centre.aspx" target="_blank" rel="noopener noreferrer">WorkBC office</a>, 
                     an application guide is also available <a href="https://workbc.ca/getmedia/2e608021-09f1-4429-885c-155e6e4bd771/WS-Guide-Nov12.pdf.aspx" target="_blank" rel="noopener noreferrer">here</a>.
