38e8441f0fbb403bbb25d86c4f14f31c0be676d0
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2021-02-10 09:04:07 -0800

    updated bolding on alert

--- client/src/components/Forms/HaveEmployeeForm/HaveEmployeeStep2.js
+++ client/src/components/Forms/HaveEmployeeForm/HaveEmployeeStep2.js
@@ -169,7 +169,7 @@ class HaveEmployeeStep2 extends Component {
                                 <div className="form-row">
                                     {this.showWarningDomain}
                                     <div className="alert alert-primary" role="alert">
-                                        Each identified employee email address must be unique and <b>must</b> not match your business email.  The employee will receive an email outlining the next steps required to complete the Wage Subsidy application
+                                        Each identified employee email address must be unique and <b>must not</b> match your business email.  The employee will receive an email outlining the next steps required to complete the Wage Subsidy application
                                     </div>
                                     {this.employeeEmails(Entry)}
                                 </div>
