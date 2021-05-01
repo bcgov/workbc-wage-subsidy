2c6a36ad1053f1f4772ef18f0516233a26205fc5
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2021-04-30 17:33:25 -0700

    revert to 3 minutes

--- cron/app.js
+++ cron/app.js
@@ -710,7 +710,7 @@ async function sendEmailsNeedEmployee(values) {
   }
 }
 
-cron.schedule('*/1 * * * *', async function() {
+cron.schedule('*/3 * * * *', async function() {
     console.log('running a task every 3 minutes');
     //console.log('running a task every 10 seconds');
     /*
