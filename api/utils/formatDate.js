ad8b7d662985e8dda73a55a35957caba06773a41
Sid McLaughlin <sidmclaughlin@gmail.com>
2022-10-18 10:30:19 -0700

    initial commit:

--- api/utils/formatDate.js
+++
@@ -1,9 +0,0 @@
-function formatDate(date){
-    if (date !== "" && typeof date !== "undefined" && date !== null){
-        return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
-    } else {
-        return ""
-    }
-}
-
-module.exports = formatDate
--- api/utils/strings.js
+++
@@ -1,8 +0,0 @@
-
-const Strings = {};
-
-Strings.orEmpty = function (entity) {
-  return entity || "";
-};
-
-module.exports = Strings;
