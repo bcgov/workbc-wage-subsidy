23b96d4a1f0bf75a8a1c0cbd811f9bc678a8bfa4
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2022-10-24 10:40:50 -0700

    add sort

--- packages/admin-api/src/services/claims.service.ts
+++ packages/admin-api/src/services/claims.service.ts
@@ -1,21 +1,24 @@
 /* eslint-disable import/prefer-default-export */
-import { knex } from '../config/db-config'
+import { knex } from "../config/db-config"
 
-export const getAllClaims = async (perPage: number, currPage: number, filters: any) => {
-  const claims = await knex('wage_subsidy_claim_form')
-    .modify((queryBuilder: any) => {
-      if (filters.applicationstatus) {
-        queryBuilder.whereIn("applicationstatus", filters.applicationstatus)
-      }
-      if (filters.catchmentno){
-        queryBuilder.where("catchmentno", Number(filters.catchmentno))
-      }
-    })
-    .paginate({ perPage, currentPage: currPage })
-  return claims
+export const getAllClaims = async (perPage: number, currPage: number, filters: any, sort: any) => {
+    const claims = await knex("wage_subsidy_claim_form")
+        .modify((queryBuilder: any) => {
+            if (filters.applicationstatus) {
+                queryBuilder.whereIn("applicationstatus", filters.applicationstatus)
+            }
+            if (filters.catchmentno) {
+                queryBuilder.where("catchmentno", Number(filters.catchmentno))
+            }
+            if (sort) {
+                queryBuilder.orderBy(sort[0], sort[1])
+            }
+        })
+        .paginate({ perPage, currentPage: currPage })
+    return claims
 }
 
 export const getClaimsByCatchment = async (ca: number[]) => {
-  const claims = await knex('wage_subsidy_claim_form').where((builder: any) => builder.whereIn('catchmentno', ca))
-  return claims
+    const claims = await knex("wage_subsidy_claim_form").where((builder: any) => builder.whereIn("catchmentno", ca))
+    return claims
 }
