11c97dc2d848b79de092fbaf71d3c9183d5ca319
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2022-11-10 11:26:34 -0800

    add morgan logging

--- packages/form-api/src/app.ts
+++ packages/form-api/src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors"
 import express from "express"
 import helmet from "helmet"
+import morgan from "morgan"
 import claimRoute from "./routes/claim.route"
 import wageRoute from "./routes/wage.route"
 
@@ -12,10 +13,21 @@ const corsOptions = {
 
 const app = express()
 
+app.use(morgan("[:date] :method :url :status :res[content-length] - :remote-addr - :response-time ms"))
+app.set("trust proxy", "loopback, linklocal, uniquelocal")
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors(corsOptions))
-app.use(helmet())
+app.use(
+    helmet.contentSecurityPolicy({
+        useDefaults: true,
+        directives: {
+            "form-action": ["'none'"],
+            "style-src": ["'none'"],
+            "font-src": ["'none'"]
+        }
+    })
+)
 
 app.use("/", wageRoute)
 app.use("/", claimRoute)
