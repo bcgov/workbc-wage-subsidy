2b5c6f6b7cb135ae7720e5aee075e86990bc8e10
npham49 <brian.d.pham@gov.bc.ca>
2023-02-09 11:40:30 -0800

    changed openshift build template for conflicts with v1 wage-subsidy

--- packages/admin-api/jest.config.js
+++ packages/admin-api/jest.config.js
@@ -1,17 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testEnvironmentOptions: {
-    NODE_ENV: 'test',
-  },
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  moduleFileExtensions: ["ts", "js"],
-  testMatch: ['**/**/*.test.ts'],
-  verbose: true,
-  forceExit: true,
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-  },
-  // clearMocks: true,
-};
+    preset: "ts-jest",
+    testEnvironment: "node",
+    testEnvironmentOptions: {
+        NODE_ENV: "test"
+    },
+    testPathIgnorePatterns: ["/dist/", ".js", ".d.ts"],
+    moduleFileExtensions: ["ts", "js"],
+    testMatch: ["**/**/*.test.ts"],
+    verbose: true,
+    forceExit: true,
+    transform: {
+        "^.+\\.(ts|tsx)?$": "ts-jest"
+    }
+    // clearMocks: true,
+}
