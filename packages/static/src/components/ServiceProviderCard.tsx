8620ce5e39b2d7fc8191e1bb9081f29695db4150
Spencer Davis <9757975+strdavis@users.noreply.github.com>
2023-06-02 15:42:42 -0700

    Implement basic modals. Correct card child positions.

--- packages/static/src/components/ServiceProviderCard.tsx
+++ packages/static/src/components/ServiceProviderCard.tsx
@@ -1,16 +1,29 @@
+import Box from "@mui/material/Box"
+import Grid from "@mui/material/Grid"
 import Card from "./common/Card/Card"
 
 const ServiceProviderCard = () => (
     <Card>
-        <h2>Are you a WorkBC Service Provider?</h2>
-        <p>If you are a WorkBC Service Provider, log in here</p>
-        <button
-            className="BC-Gov-PrimaryButton"
-            type="button"
-            onClick={() => window.open("https://wage-sub-dev-sp.es.workbc.ca")}
-        >
-            Service Provider Login
-        </button>
+        <Grid container direction="column">
+            <Grid item>
+                <h2>Are you a WorkBC Service Provider?</h2>
+                <p>If you are a WorkBC Service Provider, log in here</p>
+            </Grid>
+            <Grid item>
+                <Box display="flex" justifyContent="right">
+                    <img width="250px" src="/employer.svg" alt="Employer logo" />
+                </Box>
+            </Grid>
+            <Grid item>
+                <button
+                    className="BC-Gov-PrimaryButton"
+                    type="button"
+                    onClick={() => window.open("https://wage-sub-dev-sp.es.workbc.ca")}
+                >
+                    Service Provider Login
+                </button>
+            </Grid>
+        </Grid>
     </Card>
 )
 
