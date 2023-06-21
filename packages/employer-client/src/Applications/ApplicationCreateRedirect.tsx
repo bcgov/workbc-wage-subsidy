9701cb48169bcdb0b6832be77394a7a327b21564
Spencer Davis <9757975+strdavis@users.noreply.github.com>
2023-06-21 16:35:56 -0700

    Implement employer claim landing page, claim application selection page, and claims list. Polish styling.

--- packages/employer-client/src/Applications/ApplicationCreateRedirect.tsx
+++ packages/employer-client/src/Applications/ApplicationCreateRedirect.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react"
-import { useRedirect } from "react-admin"
+import { useRedirect, useStore } from "react-admin"
+import { applicationStatusFilters } from "./ApplicationList"
 
 export const ApplicationCreateRedirect = () => {
     const redirect = useRedirect()
+    const [, setStatusFilter] = useStore<any>("resources.applications.list.statusFilter", applicationStatusFilters.All)
 
     useEffect(() => {
+        setStatusFilter(applicationStatusFilters["All"])
         redirect("create", "wage")
-    }, [redirect])
+    }, [setStatusFilter, redirect])
 
     return <span />
 }
