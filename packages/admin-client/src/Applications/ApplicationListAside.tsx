45417f0692e223463839d8a4212f064f47b94c48
Spencer Davis <9757975+strdavis@users.noreply.github.com>
2023-07-17 16:57:41 -0700

    Set status filter to All when catchment is changed.
    Improve UX for catchment selection.

--- packages/admin-client/src/Applications/ApplicationListAside.tsx
+++ packages/admin-client/src/Applications/ApplicationListAside.tsx
@@ -1,6 +1,6 @@
 import { Box, ListItemText, MenuItem, MenuList } from "@mui/material"
 import isEqual from "lodash/isEqual"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { Count, useListContext } from "react-admin"
 import { CatchmentContext } from "../common/contexts/CatchmentContext/CatchmentContext"
 import { applicationStatusFilters } from "./ApplicationList"
@@ -8,6 +8,11 @@ import { applicationStatusFilters } from "./ApplicationList"
 export const ApplicationListAside = () => {
     const cc = useContext(CatchmentContext)
     const { filterValues, displayedFilters, setFilters } = useListContext()
+
+    useEffect(() => {
+        // When catchment is changed, set status filter to 'All'.
+        setFilters({ ...applicationStatusFilters["All"], ...{ catchmentno: cc.catchment.id } }, displayedFilters)
+    }, [cc.catchment])
 
     return (
         <Box width={200} mr={1} mt={7} flexShrink={0} order={-1}>
