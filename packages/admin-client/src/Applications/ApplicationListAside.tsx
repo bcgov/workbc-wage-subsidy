99d2313bae8b2594383fcec1ed92fdf6022403da
Spencer Davis <9757975+strdavis@users.noreply.github.com>
2023-07-17 12:15:01 -0700

    Implement exact styling as in mockup. Facilitate future catchment sync across lists.
    Replace CustomSelectInput with CatchmentDropdown. Make catchment dropdown state depend on catchment context, so catchment can be synced across lists. Pass CatchmentDropdown as ListAction to enable right-alignment. Ensure dropdown always defaults to first catchment in list, even after logging out and logging back in. Fix catchment bugs. In ListAside use ListContext rather than store to update list filter. Always display Select Input as CatchmentDropdown label.

--- packages/admin-client/src/Applications/ApplicationListAside.tsx
+++ packages/admin-client/src/Applications/ApplicationListAside.tsx
@@ -1,17 +1,13 @@
 import { Box, ListItemText, MenuItem, MenuList } from "@mui/material"
 import isEqual from "lodash/isEqual"
 import { useContext } from "react"
-import { Count, useStore } from "react-admin"
+import { Count, useListContext } from "react-admin"
 import { CatchmentContext } from "../common/contexts/CatchmentContext/CatchmentContext"
 import { applicationStatusFilters } from "./ApplicationList"
 
 export const ApplicationListAside = () => {
     const cc = useContext(CatchmentContext)
-
-    const [listFilter, setListFilter] = useStore("resources.applications.list.listFilter", {
-        ...applicationStatusFilters["All"],
-        ...{ catchmentno: cc.catchment.id }
-    })
+    const { filterValues, displayedFilters, setFilters } = useListContext()
 
     return (
         <Box width={200} mr={1} mt={7} flexShrink={0} order={-1}>
@@ -20,9 +16,12 @@ export const ApplicationListAside = () => {
                     <MenuItem
                         key={key}
                         onClick={() => {
-                            setListFilter({ ...applicationStatusFilters[key], ...{ catchmentno: cc.catchment.id } })
+                            setFilters(
+                                { ...applicationStatusFilters[key], ...{ catchmentno: cc.catchment.id } },
+                                displayedFilters
+                            )
                         }}
-                        selected={isEqual(listFilter.label, applicationStatusFilters[key].label)}
+                        selected={isEqual(filterValues.label, applicationStatusFilters[key].label)}
                     >
                         <ListItemText>{applicationStatusFilters[key].label}</ListItemText>
                         <Count
