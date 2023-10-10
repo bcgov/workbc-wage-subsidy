db51cea2673a9503fe3916128457976a1e7a0701
Branko Bajic <eisoftware@mailfence.com>
2023-10-10 16:53:07 -0700

    Form loading indicators

--- packages/employer-client/src/Form/ViewForm.tsx
+++ packages/employer-client/src/Form/ViewForm.tsx
@@ -1,13 +1,28 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
+import React from "react"
+import { useState } from "react"
 import { Box, Button, Tooltip } from "@mui/material"
 import { useParams } from "react-router"
 import { COLOURS } from "../Colours"
 import BackButton from "../common/components/BackButton/BackButton"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUpRightFromSquare } from "@fortawesome/pro-solid-svg-icons"
+import { Loading, useRefresh } from "react-admin"
 
 export const ViewForm = () => {
     const { urlType, resource, formId } = useParams()
+    const [numLoads, setNumLoads] = useState(0)
+    const [loading, setLoading] = useState(true)
+    const refresh = useRefresh()
+
+    React.useEffect(() => {
+        if (numLoads === 2) {
+            refresh()
+            setLoading(false)
+            setNumLoads(0)
+        }
+    }, [numLoads])
+
     if (!urlType || !resource || !formId) {
         return <span />
     }
@@ -17,56 +32,62 @@ export const ViewForm = () => {
     if (urlType === "Draft") formUrl = process.env.REACT_APP_DRAFT_URL + formId
 
     return (
-        <Box style={{ position: "relative", marginTop: 7 }}>
-            <div
-                style={{
-                    borderBottom: "solid 2px " + COLOURS.MEDIUMGREY,
-                    backgroundColor: "white",
-                    width: "100%",
-                    height: "5em",
-                    position: "absolute",
-                    zIndex: 1
-                }}
-            >
-                <Box style={{ display: "flex", marginTop: "1em" }}>
-                    <BackButton resource={resource} />
-                    <Box style={{ display: "flex", width: "100%", justifyContent: "right" }}>
-                        <Tooltip title="Open form in new tab">
-                            <Button
-                                onClick={(event) => {
-                                    window.open(formUrl)
-                                }}
-                                sx={{ minWidth: "4em" }}
-                                aria-label="Open form in new tab"
-                            >
-                                <FontAwesomeIcon
-                                    icon={faUpRightFromSquare}
-                                    size="xl"
-                                    style={{ color: COLOURS.LIGHTBLUE_TEXT, padding: "0.65em 0em" }}
-                                />
-                            </Button>
-                        </Tooltip>
+        <div>
+            {loading && <Loading></Loading>}
+            <Box hidden={loading} style={{ position: "relative", marginTop: 7 }}>
+                <div
+                    style={{
+                        borderBottom: "solid 2px " + COLOURS.MEDIUMGREY,
+                        backgroundColor: "white",
+                        width: "100%",
+                        height: "5em",
+                        position: "absolute",
+                        zIndex: 1
+                    }}
+                >
+                    <Box style={{ display: "flex", marginTop: "1em" }}>
+                        <BackButton resource={resource} />
+                        <Box style={{ display: "flex", width: "100%", justifyContent: "right" }}>
+                            <Tooltip title="Open form in new tab">
+                                <Button
+                                    onClick={(event) => {
+                                        window.open(formUrl)
+                                    }}
+                                    sx={{ minWidth: "4em" }}
+                                    aria-label="Open form in new tab"
+                                >
+                                    <FontAwesomeIcon
+                                        icon={faUpRightFromSquare}
+                                        size="xl"
+                                        style={{ color: COLOURS.LIGHTBLUE_TEXT, padding: "0.65em 0em" }}
+                                    />
+                                </Button>
+                            </Tooltip>
+                        </Box>
                     </Box>
-                </Box>
-            </div>
-            <iframe
-                src={formUrl}
-                style={{ border: "solid 2px " + COLOURS.MEDIUMGREY, width: "100%", height: "55em" }}
-            />
-            <div
-                className="bottom-hider"
-                style={{
-                    borderTop: "solid 2px " + COLOURS.MEDIUMGREY,
-                    backgroundColor: "white",
-                    width: "100%",
-                    height: "110px",
-                    position: "absolute",
-                    zIndex: 1,
-                    bottom: 0
-                }}
-            >
-                &nbsp;
-            </div>
-        </Box>
+                </div>
+                <iframe
+                    src={formUrl}
+                    style={{ border: "solid 2px " + COLOURS.MEDIUMGREY, width: "100%", height: "55em" }}
+                    onLoad={(e) => {
+                        setNumLoads((numLoads) => numLoads + 1)
+                    }}
+                />
+                <div
+                    className="bottom-hider"
+                    style={{
+                        borderTop: "solid 2px " + COLOURS.MEDIUMGREY,
+                        backgroundColor: "white",
+                        width: "100%",
+                        height: "110px",
+                        position: "absolute",
+                        zIndex: 1,
+                        bottom: 0
+                    }}
+                >
+                    &nbsp;
+                </div>
+            </Box>
+        </div>
     )
 }
