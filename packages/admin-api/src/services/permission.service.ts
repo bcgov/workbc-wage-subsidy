cfdc301625c656dc76dda8b08078503787e14fa5
Branko Bajic <eisoftware@mailfence.com>
2024-01-09 14:12:18 -0700

    force axios 1.6.2, put auth object back in

--- packages/admin-api/src/services/permission.service.ts
+++ packages/admin-api/src/services/permission.service.ts
@@ -5,6 +5,7 @@ import axios from "axios"
 
 export const getPermission = async (guid: string, isIDIR: boolean) => {
     const url = process.env.SAM_API_URL as string
+    console.log("SAM API USERNAME: ", `${process.env.SAM_API_USERNAME}`)
     const response = await axios({
         method: "GET",
         url,
@@ -12,6 +13,10 @@ export const getPermission = async (guid: string, isIDIR: boolean) => {
             userGUID: guid,
             // eslint-disable-next-line object-shorthand
             isIDIR: isIDIR
+        },
+        auth: {
+            username: process.env.SAM_API_USERNAME as string,
+            password: process.env.SAM_API_PASSWORD as string
         }
     }).catch((error) => {
         console.log(error)
