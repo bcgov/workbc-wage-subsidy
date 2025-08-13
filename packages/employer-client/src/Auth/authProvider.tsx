469eb924f500c8eeeb3a579934c9abb4d94bbfc6
makskononenko <maksym.kononenko@gov.bc.ca>
2025-08-13 14:11:38 -0700

    try7

--- packages/employer-client/src/Auth/authProvider.tsx
+++ packages/employer-client/src/Auth/authProvider.tsx
@@ -17,7 +17,7 @@ const useAuthProvider = (clientID: string) => {
             localStorage.removeItem("permissions")
             localStorage.removeItem("access")
             localStorage.clear()
-            return keycloak.logout({ redirectUri: `${absolutePath}logout-success` })
+            return keycloak.logout({ redirectUri: `${absolutePath}logout-success?test=${new Date().getTime()}` })
         },
         getIdentity: () => {
             if (keycloak.token) {
