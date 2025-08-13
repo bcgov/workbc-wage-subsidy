5ca9a82c6434ebd7c3712830380bbeea4ac719fa
Branko Bajic <eisoftware@mailfence.com>
2025-08-12 18:35:40 -0700

    implement NearestCentres address endpoint

--- packages/employer-api/src/controllers/address.controller.ts
+++ packages/employer-api/src/controllers/address.controller.ts
@@ -6,9 +6,20 @@ import * as geocoderService from "../services/geocoder.service"
 export const getAddressValidation = async (req: express.Request, res: express.Response) => {
     try {
         const { address, city, province } = req.body
-        console.log(address, city, province)
+        console.log("validating address: ", address, city, province)
         const addressValidation = await geocoderService.geocodeAddress(address, city, province)
         return res.status(200).send(addressValidation)
+    } catch (e: unknown) {
+        console.log(e)
+        return res.status(500).send("Server Error")
+    }
+}
+export const getNearestCentres = async (req: express.Request, res: express.Response) => {
+    try {
+        const { address, city, province } = req.body
+        console.log("getting nearest centres for: ", address, city, province)
+        const closestCentres = await geocoderService.calculateNearestCentres(address, city, province, 3)
+        return res.status(200).send(closestCentres)
     } catch (e: unknown) {
         console.log(e)
         return res.status(500).send("Server Error")
--- packages/employer-api/src/routes/address.route.ts
+++ packages/employer-api/src/routes/address.route.ts
@@ -4,5 +4,6 @@ import * as addressController from "../controllers/address.controller"
 const router = express.Router()
 
 router.post("/validate", addressController.getAddressValidation)
+router.post("/nearestCentres", addressController.getNearestCentres)
 
 export default router
