6a87838063bfc8c8dfecec6bd7a4559437047047
makskononenko <maksym.kononenko@gov.bc.ca>
2025-08-19 14:32:03 -0700

    try29

--- packages/employer-client/src/UserProfileModal.tsx
+++ packages/employer-client/src/UserProfileModal.tsx
@@ -105,10 +105,10 @@ const UserProfileModal: React.FC<UserProfileModalProps> = ({ isOpen, onRequestCl
     }, [isOpen])
 
     useEffect(() => {
-        if (identity && localStorage.getItem("token")) {
+        if (identity && !ec.profileExists) {
             createProfileIfNotExists(identity)
         }
-    }, [createProfileIfNotExists, identity])
+    }, [createProfileIfNotExists, ec.profileExists, identity])
 
     return (
         <>
