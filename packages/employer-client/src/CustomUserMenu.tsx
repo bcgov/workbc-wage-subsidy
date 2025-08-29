6ae68d8e1b6e8b68e6b6e8da193cba1d462013f0
makskononenko <maksym.kononenko@gov.bc.ca>
2025-08-29 13:02:45 -0700

    try 1 more thing

--- packages/employer-client/src/CustomUserMenu.tsx
+++ packages/employer-client/src/CustomUserMenu.tsx
@@ -6,6 +6,7 @@ import ListItemText from "@mui/material/ListItemText"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUserPen, faPowerOff } from "@fortawesome/pro-solid-svg-icons"
 import { setEnvVariables } from "./utils/index"
+import { useKeycloak } from "@react-keycloak/web"
 
 interface EditProfileMenuItemProps {
     openModal: () => void
@@ -36,13 +37,14 @@ interface CustomUserMenuProps {
 export const CustomUserMenu: React.FC<CustomUserMenuProps> = ({ openModal }) => {
     const { absolutePath } = setEnvVariables()
     const logout = useLogout()
+    const { keycloak } = useKeycloak()
 
     return (
         <UserMenu>
             <EditProfileMenuItem openModal={openModal} />
             <Logout
                 onClick={() => {
-                    logout(null, `${absolutePath}logout-success`, false)
+                    keycloak.logout().then(() => logout(null, `${absolutePath}logout-success`, false))
                 }}
                 icon={<FontAwesomeIcon icon={faPowerOff} size="lg" style={{ color: "black" }} tabIndex={0} />}
             />
