c6f872e01e7bfab68cce34ab52b9bb6dac4f8566
Branko Bajic <eisoftware@mailfence.com>
2025-09-03 11:32:57 -0700

    try removing padding from faCopy icon

--- packages/admin-client/src/Claims/ClaimList.tsx
+++ packages/admin-client/src/Claims/ClaimList.tsx
@@ -132,7 +132,7 @@ export const ClaimList = (props: any) => {
                                                 >
                                                     <FontAwesomeIcon
                                                         icon={faCopy}
-                                                        style={{ color: COLOURS.LIGHTBLUE_TEXT, padding: "0 0.5rem" }}
+                                                        style={{ color: COLOURS.LIGHTBLUE_TEXT }}
                                                     />
                                                 </Button>
                                             </Tooltip>
