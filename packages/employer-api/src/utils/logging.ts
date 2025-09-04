95e101136bf62bebcec3953054e6568a5f68078b
makskononenko <maksym.kononenko@gov.bc.ca>
2025-09-04 10:26:16 -0700

    clean up

--- packages/employer-api/src/utils/logging.ts
+++ packages/employer-api/src/utils/logging.ts
@@ -1,4 +1,4 @@
-import { maskJSON2, maskStringV2 } from "maskdata"
+import { maskStringV2 } from "maskdata"
 
 const IDMaskingConfig = {
     maskWith: "*",
@@ -13,178 +13,6 @@ const AddressMaskingConfig = {
     unmaskedEndCharacters: 0
 }
 
-const JSONMaskingConfig = {
-    cardMaskOptions: {
-        maskWith: "*",
-        unmaskedStartDigits: 4,
-        unmaskedEndDigits: 1
-    },
-    cardFields: [],
-
-    emailMaskOptions: {
-        maskWith: "*",
-        unmaskedStartCharactersBeforeAt: 3,
-        unmaskedEndCharactersAfterAt: 3,
-        maskAtTheRate: false
-    },
-    emailFields: [
-        "Email",
-        "email",
-        "baseProfile.email",
-        "requestObject.ApplicantEmail",
-        "requestObject.Email",
-        "userInfo.email",
-        "applicationJSON.Answers.ApplicantEmail"
-    ],
-
-    passwordMaskOptions: {
-        maskWith: "*",
-        maxMaskedCharacters: 16,
-        fixedOutputLength: undefined,
-        unmaskedStartCharacters: 0,
-        unmaskedEndCharacters: 0
-    },
-    passwordFields: [],
-
-    phoneMaskOptions: {
-        maskWith: "*",
-        unmaskedStartDigits: 4,
-        unmaskedEndDigits: 1
-    },
-    phoneFields: [
-        "mobileNumber",
-        "phn",
-        "homeNumber",
-        "CaseContactInfoResponse.ContactInformation.HomePhone",
-        "CaseContactInfoResponse.ContactInformation.WorkPhone",
-        "CaseContactInfoResponse.ContactInformation.MessagePhone",
-        "CaseContactInfoResponse.ContactInformation.CellPhone",
-        "applicationJSON.Answers.ApplicantHomePhone",
-        "applicationJSON.Answers.ApplicantCellPhone"
-    ],
-
-    stringMaskOptions: {
-        maskWith: "*",
-        maskOnlyFirstOccurance: false,
-        values: [],
-        maskAll: false,
-        maskSpace: true
-    },
-    stringFields: ["baseProfile.email"],
-
-    uuidMaskOptions: {
-        maskWith: "*",
-        unmaskedStartCharacters: 0,
-        unmaskedEndCharacters: 0
-    },
-    uuidFields: [],
-
-    jwtMaskOptions: {
-        maskWith: "*",
-        maxMaskedCharacters: 20,
-        maskDot: true,
-        maskHeader: true,
-        maskPayload: true,
-        maskSignature: true
-    },
-    jwtFields: ["SigningToken"],
-
-    genericStrings: [
-        {
-            // keys/tokens useful for debugging but that need to be masked //
-            fields: [
-                "deviceToken",
-                "userID",
-                "fcmToken",
-                "GuID",
-                "Guid",
-                "GuID",
-                "userGUID",
-                "Username",
-                "key",
-                "url",
-                "clientBCeID",
-                "BCSC_DID",
-                "CaseContactInfoResponse.ContactInformation.BCeIDGUID",
-                "CaseContactInfoResponse.ContactInformation.BCSCDID",
-                "MobileDevices.*",
-                "requestObject.ApplicantGUID",
-                "requestObject.ApplicantBcscDID",
-                "requestObject.GuID",
-                "applicationJSON.Answers.ApplicantBcscDID"
-            ],
-            config: {
-                maskWith: "*",
-                fixedOutputLength: 20,
-                unmaskedStartCharacters: 5,
-                unmaskedEndCharacters: 0
-            }
-        },
-        {
-            // SIN/DOB //
-            fields: [
-                "Sin",
-                "sin",
-                "BirthDate",
-                "dob",
-                "baseProfile.birthDate",
-                "CaseContactInfoResponse.ContactInformation.SIN",
-                "CaseContactInfoResponse.ContactInformation.DOB",
-                "requestObject.ApplicantSIN",
-                "requestObject.ApplicantBirthDate",
-                "requestObject.BirthDate",
-                "requestObject.Sin",
-                "applicationJSON.Answers.ApplicantSin",
-                "actionPlans.*"
-            ],
-            config: {
-                maskWith: "*",
-                unmaskedStartCharacters: 3
-            }
-        },
-        {
-            // Personal Information //
-            fields: [
-                "FirstName",
-                "firstName",
-                "LastName",
-                "lastName",
-                "MiddleName",
-                "middleName",
-                "StreetAddressLine1",
-                "applicationJSON.Answers.ApplicantBirthDate.*",
-                "applicationJSON.Answers.PrimaryAddress.*",
-                "applicationJSON.Answers.ApplicantGender",
-                "CaseContactInfoResponse.ContactInformation.ContactAddress",
-                "userInfo.firstName",
-                "userInfo.lastName",
-                "userInfo.middleName",
-                "applicationJSON.Answers.ApplicantFirstName",
-                "applicationJSON.Answers.ApplicantLastName",
-                "applicationJSON.Answers.ApplicantMiddleName",
-                "requestObject.ApplicantFirstName",
-                "requestObject.ApplicantLastName",
-                "requestObject.ApplicantMiddleName",
-                "requestObject.FirstName",
-                "requestObject.LastName",
-                "requestObject.MiddleName",
-                "requestObject.PhoneNumbers.*",
-                "requestObject.phn",
-                "CaseContactInfoResponse.ContactInformation.FirstName",
-                "CaseContactInfoResponse.ContactInformation.LastName",
-                "CaseContactInfoResponse.ContactInformation.MiddleName",
-                "CaseContactInfoResponse.ContactInformation.ContactAddress",
-                "baseProfile.firstName",
-                "baseProfile.lastName",
-                "baseProfile.middleName"
-            ],
-            config: {
-                maskWith: "*"
-            }
-        }
-    ]
-}
-
 const EmailMaskingConfig = {
     maskWith: "*",
     fixedOutputLength: 20,
@@ -194,19 +22,8 @@ const EmailMaskingConfig = {
 
 const maskID = (ID: string) => maskStringV2(ID, IDMaskingConfig)
 
-const maskJSON = (log: unknown): string | undefined => {
-    try {
-        if (typeof log === "object" && log !== null) {
-            return JSON.stringify(maskJSON2(log, JSONMaskingConfig))
-        }
-    } catch (error) {
-        return String(log)
-    }
-    return String(log)
-}
-
 const maskAddress = (address: string) => maskStringV2(address, AddressMaskingConfig)
 
 const maskEmail = (email: string) => maskStringV2(email, EmailMaskingConfig)
 
-export { maskID, maskJSON, maskAddress, maskEmail }
+export { maskID, maskAddress, maskEmail }
